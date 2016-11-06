350d2cad1d323a049b63e5f308be7adfe3942478
calaca <lorena.calaca.od@gmail.com>
2016-11-06 02:09:52 -0200

    fix: tried to fix double stream showing on screen. Still not working.

--- js/app.js
+++ js/app.js
@@ -10,6 +10,8 @@ var channel  = "https://api.twitch.tv/kraken/channels/";
 var stream   = "https://api.twitch.tv/kraken/streams/";
 var channels = new Array();
 var streams  = new Array();
+var online   = new Array();
+// var offline  = new Array();
 
 // Get all channel and stream api links for every user
 var len = users.length;
@@ -20,22 +22,27 @@ for (var i = 0; i < len; i++) {
 }
 
 $(document).ready(function() {
-  // TODO: show channel and stream information on screen
-  // Channel information
   for (var i = 0; i < len; i++) {
-    $.getJSON(channels[i], function(json) {
-      $('.streams-wrapper').append("<div class=\"stream\"><img src='" + json.logo + "' alt='" + json.display_name + " logo'><div class=\"stream-info\"><p class=\"streamer-name\"><a href='https://www.twitch.tv/" + json.name + "'>" + json.display_name + "</a></p></div></div>");
-      // console.log("name: " + json.display_name);
-      // console.log("logo: " + json.logo);
-      // console.log("channel link: " + "https://www.twitch.tv/" + json.name);
-    });
     // Stream information
     $.getJSON(streams[i], function(json) {
       if (json.stream !== null) {
         $('.streams-wrapper').append("<div class=\"stream\"><img src='" + json.stream.channel.logo + "' alt='" + json.display_name + " logo'><div class=\"stream-info\"><p class=\"streamer-name\"><a href=" + json.stream.channel.url + ">" + json.stream.channel.display_name + "</a></p><p class=\"streamer-game\">" + json.stream.channel.game + "</p><p class=\"streamer-status\">" + json.stream.channel.status + "</p></div></div>");
+        online[i] = json.stream.channel.display_name;
         // console.log(json);
         // console.log("game: " + json.stream.channel.game);
         // console.log("status: " + json.stream.channel.status);
+      }
+    });
+    // Channel information
+    $.getJSON(channels[i], function(json) {
+      // TODO: Find a way to show only streams that are offline in here
+      if(online[i] !== json.display_name) {
+        $('.streams-wrapper').append("<div class=\"stream\"><img src='" + json.logo + "' alt='" + json.display_name + " logo'><div class=\"stream-info\"><p class=\"streamer-name\"><a href='" + json.url + "'>" + json.display_name + "</a></p></div></div>");
+        offline[i] = json.display_name;
+        // console.log(json);
+        // console.log("name: " + json.display_name);
+        // console.log("logo: " + json.logo);
+        // console.log("channel link: " + "https://www.twitch.tv/" + json.name);
       }
     });
   }
