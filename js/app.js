88fdd97276967ccf729646e843ad5425395535a5
calaca <lorena.calaca.od@gmail.com>
2016-11-07 02:29:59 -0200

    feat: update app.js but still need to fix problems regarding asynchronism

--- js/app.js
+++ js/app.js
@@ -4,18 +4,40 @@ Channel info: https://api.twitch.tv/kraken/channels/nomadtv?client_id=puyss7akb1
 Stream info: https://api.twitch.tv/kraken/streams/nomadtv?client_id=puyss7akb1pw4295zxh8m5bs2bxugrm
 */
 // TODO: get all users to lower case, compare to input search in lower case, show channel if exists show notification if doesn't.
-var users    = ["nomadtv", "OgamingSC2", "chaotictabris", "freecodecamp", "storbeck", "blizzard", "RobotCaleb", "noobs2ninjas"];
-var key      = "?client_id=puyss7akb1pw4295zxh8m5bs2bxugrm";
-var channel  = "https://api.twitch.tv/kraken/channels/";
-var stream   = "https://api.twitch.tv/kraken/streams/";
-var channels = new Array();
-var streams  = new Array();
-var online   = new Array();
-// var offline  = new Array();
+var users     = ["nomadtv", "OgamingSC2", "chaotictabris", "freecodecamp", "storbeck", "blizzard", "RobotCaleb", "noobs2ninjas"];
+var key       = "?client_id=puyss7akb1pw4295zxh8m5bs2bxugrm";
+var channel   = "https://api.twitch.tv/kraken/channels/";
+var stream    = "https://api.twitch.tv/kraken/streams/";
+var channels  = [];
+var streams   = [];
+var online    = [];
+var populated = false;
+var len       = users.length;
+var interval;
 
-// Get all channel and stream api links for every user
-var len = users.length;
+// TODO: Finish these functions
+function printAll(o) {
+  console.log(o);
+  console.log(o[1].status);
+};
 
+function printOnline(online) {
+
+};
+
+function printOffline(online) {
+
+};
+
+// Checks if array is fully populated
+interval = setInterval(function(){
+  if (populated) {
+    printAll(online);
+    clearInterval(interval);
+  }
+}, 100);
+
+// Creates all api links needed
 for (var i = 0; i < len; i++) {
   channels[i] = channel + users[i] + key;
   streams[i]  = stream + users[i] + key;
@@ -23,27 +45,39 @@ for (var i = 0; i < len; i++) {
 
 $(document).ready(function() {
   for (var i = 0; i < len; i++) {
-    // Stream information
+
+    // Checks which streams are online/offline and create the corresponding object
     $.getJSON(streams[i], function(json) {
-      if (json.stream !== null) {
-        $('.streams-wrapper').append("<div class=\"stream\"><img src='" + json.stream.channel.logo + "' alt='" + json.display_name + " logo'><div class=\"stream-info\"><p class=\"streamer-name\"><a href=" + json.stream.channel.url + ">" + json.stream.channel.display_name + "</a></p><p class=\"streamer-game\">" + json.stream.channel.game + "</p><p class=\"streamer-status\">" + json.stream.channel.status + "</p></div></div>");
-        online[i] = json.stream.channel.display_name;
-        // console.log(json);
-        // console.log("game: " + json.stream.channel.game);
-        // console.log("status: " + json.stream.channel.status);
+      if (json.stream === null) {
+        var user = {
+          status : false,
+          data   : null
+        };
+        online.push(user);
+      } else {
+        var user = {
+          status : true,
+          data   : json.stream
+        };
+        online.push(user);
+      }
+
+      // TODO: Asynchronism is making this a nightmare
+      if (i == len - 1) {
+        populated = true;
+        console.log('yes');
       }
     });
+
     // Channel information
     $.getJSON(channels[i], function(json) {
-      // TODO: Find a way to show only streams that are offline in here
-      if(online[i] !== json.display_name) {
-        $('.streams-wrapper').append("<div class=\"stream\"><img src='" + json.logo + "' alt='" + json.display_name + " logo'><div class=\"stream-info\"><p class=\"streamer-name\"><a href='" + json.url + "'>" + json.display_name + "</a></p></div></div>");
-        offline[i] = json.display_name;
-        // console.log(json);
-        // console.log("name: " + json.display_name);
-        // console.log("logo: " + json.logo);
-        // console.log("channel link: " + "https://www.twitch.tv/" + json.name);
-      }
+      // Offline stream element
+      $('.streams-wrapper').append("<div class=\"stream\"><img src='" + json.logo + "' alt='" + json.display_name + " logo'><div class=\"stream-info\"><p class=\"streamer-name\"><a href='" + json.url + "'>" + json.display_name + "</a></p></div></div>");
+      // console.log("channel link: " + "https://www.twitch.tv/" + json.name);
     });
   }
 });
+
+
+// This is the online stream element
+// $('.streams-wrapper').append("<div class=\"stream\"><img src='" + json.stream.channel.logo + "' alt='" + json.display_name + " logo'><div class=\"stream-info\"><p class=\"streamer-name\"><a href=" + json.stream.channel.url + ">" + json.stream.channel.display_name + "</a></p><p class=\"streamer-game\">" + json.stream.channel.game + "</p><p class=\"streamer-status\">" + json.stream.channel.status + "</p></div></div>");
