732e801669f6b148ca3131e1dc2beb44ded16887
guepardo <bsinet@hotmail.com>
2016-11-09 10:53:14 -0200

    refactor 3

--- js/app.js
+++ js/app.js
@@ -1,109 +1,95 @@
-var users     = ["nomadtv", "OgamingSC2", "chaotictabris", "freecodecamp", "nvidiafrance", "blizzard", "RobotCaleb", "unitlost"];
-var key       = "?client_id=puyss7akb1pw4295zxh8m5bs2bxugrm";
-var channel   = "https://api.twitch.tv/kraken/channels/";
-var stream    = "https://api.twitch.tv/kraken/streams/";
-var channels  = [];
-var streams   = [];
-var online    = [];
-var populated = 0;
-var len       = users.length;
-var interval;
+  new Vue({
+    el: '#app-vue', 
 
-// TODO: Work on the search function
+    data:{
+      users: [], 
+      nameUsers: ['nomadtv', 'OgamingSC2', 'chaotictabris', 'freecodecamp', 'nvidiafrance', 'blizzard', 'RobotCaleb', 'unitlost'], 
+      apiKey: '?client_id=puyss7akb1pw4295zxh8m5bs2bxugrm', 
+      relativePath: 'https://api.twitch.tv/kraken/', 
+      query: '', 
+      filterStatus: 'all'
+    }, 
 
-// Prints all channels information
-function printAll(o) {
-  printOnline(o);
-  printOffline(o);
-};
+    created: function(){
+     this.updateUsers(); 
+   }, 
 
-// Prints online channels
-function printOnline(o) {
-  for (var i = 0; i < len; i++) {
-    if (online[i].status) {
-      $('.streams-wrapper').append("<div class=\"stream\"><img src='" + o[i].data.channel.logo + "' alt='" + o[i].data.display_name + " logo'><div class=\"stream-info\"><p class=\"streamer-name\"><a href=" + o[i].data.channel.url + ">" + o[i].data.channel.display_name + " <i class=\"fa fa-circle-o\" aria-hidden=\"true\"></i></a></p><p class=\"streamer-game\">" + o[i].data.channel.game + "</p><p class=\"streamer-status\">" + o[i].data.channel.status + "</p></div></div>");
-    }
-  }
-};
+   methods:{
+    getPathRequestChannels: function(userName){
+      return this.relativePath + 'channels/' + userName + this.apiKey; 
+    }, 
 
-// Prints offline channels
-function printOffline(o) {
-  for (var i = 0; i < len; i++) {
-    if (!o[i].status) {
-      $.getJSON(channels[i], function(json) {
-        $('.streams-wrapper').append("<div class=\"stream\"><img src='" + json.logo + "' alt='" + json.display_name + " logo'><div class=\"stream-info\"><p class=\"streamer-name\"><a href='" + json.url + "'>" + json.display_name + " <i class=\"fa fa-circle-o-notch\" aria-hidden=\"true\"></i></a></p></div></div>");
-      });
-    }
-  }
-};
+    getPathRequestStreams: function(userName){
+      return this.relativePath + 'streams/' + userName + this.apiKey; 
+    }, 
 
-// Prints all channels when 'all' link is clicked
-function clickAll(o) {
-  $(document).ready(function() {
-    $('#all').click(function() {
-      $('.stream').remove();
-      printAll(o);
-    });
-  });
-}
+    extractName: function(url){
+      return url.substr( url.lastIndexOf('/') + 1 )
+    }, 
 
-// Prints online channels when 'online' link is clicked
-function clickOnline(o) {
-  $(document).ready(function() {
-    $('#online').click(function() {
-      $('.stream').remove();
-      printOnline(o);
-    });
-  });
-}
-
-// Prints offline channels when 'offline' link is clicked
-function clickOffline(o) {
-  $(document).ready(function() {
-    $('#offline').click(function() {
-      $('.stream').remove();
-      printOffline(o);
-    });
-  });
-}
-
-// Checks if array is fully populated
-interval = setInterval(function() {
-  console.log('Testando o intervalo.');
-  if (populated == len) {
-    clickAll(online);
-    clickOnline(online);
-    clickOffline(online);
-    printAll(online);
-    clearInterval(interval);
-  }
-}, 100);
-
-// Creates all api links needed
-for (var i = 0; i < len; i++) {
-  channels[i] = channel + users[i] + key;
-  streams[i]  = stream + users[i] + key;
-}
-
-// Checks which streams are online/offline and create the corresponding object
-$(document).ready(function() {
-  for (var i = 0; i < len; i++) {
-    $.getJSON(streams[i]).done(function(json) {
-      if (json.stream === null) {
-        var user = {
-          status : false,
-          data   : null
-        };
-        online.push(user);
-      } else {
-        var user = {
-          status : true,
-          data   : json.stream
-        };
-        online.push(user);
+    updateUsers: function(){
+      var self = this; 
+      for(var a = 0; a < this.nameUsers.length; a++){
+        $.get(this.getPathRequestChannels(this.nameUsers[a])).
+        done(function(data){
+          var name = self.extractName(data._links.self);
+          setTimeout(function(){
+            $.get(self.getPathRequestStreams(name)).
+            done(function(sub_data){
+             var user = {
+                name   : name, 
+                online : (sub_data.stream != null), 
+                channel: data, 
+                stream: sub_data.stream
+              }; 
+              self.users.push(user); 
+            }); 
+          }, 500); 
+        }); 
       }
-    }).always(function() {
-      populated++;
-    });
-  }
-});
+    }, 
+
+    changeFilter: function(arg){
+      this.filterStatus = arg; 
+    }, 
+
+    getCssClassItem: function(arg){
+      return (arg) ? 'fa-circle-o' : 'fa-circle-o-notch'; 
+    }, 
+
+    getImgUrlByActivity: function(arg){
+      if(!arg.online)
+        return arg.channel.logo; 
+        // else
+          // return arg.
+        }
+      }, 
+
+      computed:{
+        usersComputed: function(){
+          var aux = []; 
+
+          for(var a = 0; a < this.users.length; a++){
+            if(this.users[a].name.indexOf(this.query) != -1 || this.query == ''){
+
+          // console.log(this.users[a].online); 
+
+          if(this.users[a].online && this.filterStatus == 'online'){
+            aux.push(this.users[a]); 
+            continue; 
+          }
+
+          if(!this.users[a].online && this.filterStatus == 'offline'){
+            aux.push(this.users[a]); 
+            continue; 
+          }
+
+          if(this.filterStatus == 'all')
+            aux.push(this.users[a]); 
+        }
+      }
+      return aux;
+    }, 
+  }, 
+
+}); 
